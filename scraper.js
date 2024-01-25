52d62430db830c4210fe42a320cb220d119615fe
Marcelo Magario <marcelo.magario@mzgroup.com>
2024-01-24 21:52:17 -0300

    added data and localizacao and also treated the date

--- scraper.js
+++ scraper.js
@@ -9,14 +9,17 @@ const scrapeWebsite = async () => {
     const $ = cheerio.load(response.data);
 
     // Aqui você pode utilizar seletores CSS para extrair informações específicas
-    // const data = $('.data-selector').text();
-    const swell = $('#forecast_wave_size span').text();
-    const direcao = $('#forecast_wave_direction').text();
+    const localizacao = $('.pico_header_pico_off h1').text().trim();
+    const h2Text = $('.pico_header_pico_off h2').text().trim();
+    // Extrai a data da string "Previsão atualizada Wednesday, 24/01/2024"
+    const data = h2Text.split(', ')[1];
+    const swell = $('#forecast_wave_size span').text().trim();
+    const direcao = $('#forecast_wave_direction').text().trim();
     // const vento = $('.vento-selector').text();
     // const mareAlta = $('.mare-alta-selector').text();
     // const mareBaixa = $('.mare-baixa-selector').text();
 
-    return { swell, direcao};
+    return { localizacao, data, swell, direcao};
     // return { data, swell, direcao, vento, mareAlta, mareBaixa };
   } catch (error) {
     throw new Error('Erro ao fazer a varredura no site:', error);
