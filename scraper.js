1f827bf1c65cbf57a1a4a42c80bb40044f8fb9a9
Marcelo Magario <marcelo.magario@mzgroup.com>
2024-01-25 15:36:15 -0300

    Make english standard

--- scraper.js
+++ scraper.js
@@ -8,17 +8,14 @@ const scrapeWebsite1 = async () => {
     const response = await axios.get(url);
     const $ = cheerio.load(response.data);
 
-    // Aqui você pode utilizar seletores CSS para extrair informações específicas
     const localizacao = $('.pico_header_pico_off h1').text().trim();
     const h2Text = $('.pico_header_pico_off h2').text();
-    // Extrai a data da string "Previsão atualizada Wednesday, 24/01/2024"
     const data = h2Text.split(',')[1].trim();
     const tamanho = $('#forecast_wave_size span').text().trim();
     const direcao = $('#forecast_wave_direction').text().trim();
     const vento = $('#forecast_wind_speed').text().trim().replace(/\s+/g, '');
     // const mareBaixa = $('tbody td.ce:contains("Seca") + td div div img[src*="mare_seca.png"] + div[style="line-height: 14px;"]').text().trim();
     // const mareAlta = $('.mare-alta-selector').text();
-
 
     // const regexHora = /\b\d{2}h\d{2}\b/g;
     // const horariosMareBaixa = mareBaixa.match(regexHora);
@@ -29,7 +26,7 @@ const scrapeWebsite1 = async () => {
     
   } catch (error) {
     console.error('error: ', error);
-    throw new Error('Erro ao fazer a varredura no site:', error);
+    throw new Error('Error while scraping the website 1:', error);
   }
 };
 
@@ -44,8 +41,8 @@ const scrapeWebsite2 = async () => {
 
     return { periodo };
   } catch (error) {
-    console.error('Erro no scraper do site 2:', error);
-    throw new Error('Erro ao fazer a varredura no site 2:', error);
+    console.error('Err:', error);
+    throw new Error('Error while scraping the website 1:', error);
   }
 };
 
@@ -54,15 +51,14 @@ const combineData = async () => {
     const dataFromSite1 = await scrapeWebsite1();
     const dataFromSite2 = await scrapeWebsite2();
 
-    // Combine os dados de ambos os sites
     const combinedData = { ...dataFromSite1, ...dataFromSite2 };
 
-    console.log('Dados combinados:', combinedData);
+    console.log('Combined data:', combinedData);
 
     return combinedData;
   } catch (error) {
-    console.error('Erro ao combinar dados:', error);
-    throw new Error('Erro ao combinar dados:', error);
+    console.error('Error:', error);
+    throw new Error('Error while trying to combine data:', error);
   }
 };
 
